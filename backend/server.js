fad90320d7a2b02928b76c9cc26b960a6b766bf9
sanjanayadav07 <sanjanayadav3952@gmail.com>
2026-04-18 18:15:56 +0530

    Updated backend, frontend API, and added uploads folder

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://cloud-drive-app-smoky.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
